6782bdc9b08c6d3dc8c6ca7b0926c3475dbf8699
hy2850 <hy2850@naver.com>
2022-05-05 02:27:50 +0000

    Add simple tag team test - very crude - Vanilla simple tag - Crude team : give extra reward/penalty to the predators based on the distance Result) Same as vanilla version - needs improvement

---
+++ run_simple_tag_crude_team.py
@@ -0,0 +1,268 @@
+"""
+crude_team
+: give reward/penalty to predators if they are close to/far away from each other than CLOSENESS constant
+
+Result
+: Same as vanilla simple tag
+
+Improvements
+1. Give reward/penalty only when predator catches the prey - How to detect this? 'done'?
+2. Decide reward/penalty value, not based on the distance?
+3. standard of CLOSENESS? size of the map?
+4. How to compare with other methods - reward graph?
+"""
+
+# run with conda env 'ys' or 'ysClone'
+
+import argparse
+import torch
+import os
+import numpy as np
+from gym.spaces import Box, Discrete
+from pathlib import Path
+from torch.autograd import Variable
+from tensorboardX import SummaryWriter
+from utils.make_env import make_env
+from utils.buffer import ReplayBuffer
+from utils.env_wrappers import SubprocVecEnv, DummyVecEnv
+from algorithms.attention_sac import AttentionSAC
+
+import csv
+import matplotlib.pyplot as plt
+
+import math
+
+PREDATOR = 3
+PREY = 1
+TOTAL_AGENTS = PREDATOR + PREY
+OBSTACLES = 2
+CLOSENESS = 1 # upper bound of distance that is considered as 'close' between two adversary agents
+OTHER_OFFSET = 4 + OBSTACLES * 2 # start index of the 'other agent' information in env observation
+
+def make_parallel_env(env_id, n_rollout_threads, seed):
+    def get_env_fn(rank):
+        def init_env():
+            env = make_env(env_id, discrete_action=True)
+            env.seed(seed + rank * 1000)
+            np.random.seed(seed + rank * 1000)
+            return env
+        return init_env
+    if n_rollout_threads == 1:
+        return DummyVecEnv([get_env_fn(0)])
+    else:
+        return SubprocVecEnv([get_env_fn(i) for i in range(n_rollout_threads)])
+
+def run(config):
+    f = open(f"test{config.testnum}.csv", 'w')
+    write = csv.writer(f, delimiter='\t')
+    
+    model_dir = Path('./models') / config.env_id / config.model_name
+    if not model_dir.exists():
+        run_num = 1
+    else:
+        exst_run_nums = [int(str(folder.name).split('run')[1]) for folder in
+                         model_dir.iterdir() if
+                         str(folder.name).startswith('run')]
+        if len(exst_run_nums) == 0:
+            run_num = 1
+        else:
+            run_num = max(exst_run_nums) + 1
+    curr_run = 'run%i' % run_num
+    run_dir = model_dir / curr_run
+    log_dir = run_dir / 'logs'
+    os.makedirs(log_dir)
+    logger = SummaryWriter(str(log_dir))
+
+    torch.manual_seed(run_num)
+    np.random.seed(run_num)
+    
+    env = make_parallel_env(config.env_id, config.n_rollout_threads, run_num)
+
+    model = AttentionSAC.init_from_env(env,
+                                       tau=config.tau,
+                                       pi_lr=config.pi_lr,
+                                       q_lr=config.q_lr,
+                                       gamma=config.gamma,
+                                       pol_hidden_dim=config.pol_hidden_dim,
+                                       critic_hidden_dim=config.critic_hidden_dim,
+                                       attend_heads=config.attend_heads,
+                                       reward_scale=config.reward_scale)
+
+    replay_buffer = ReplayBuffer(config.buffer_length, model.nagents,
+                                 [obsp.shape[0] for obsp in env.observation_space],
+                                 [acsp.shape[0] if isinstance(acsp, Box) else acsp.n
+                                  for acsp in env.action_space])
+    t = 0
+    for ep_i in range(0, config.n_episodes, config.n_rollout_threads):
+        print("Episodes %i-%i of %i" % (ep_i + 1,
+                                        ep_i + 1 + config.n_rollout_threads,
+                                        config.n_episodes))
+        obs = env.reset()
+
+        if config.use_gpu:
+            model.prep_training(device='gpu')
+        else:
+            model.prep_training(device='cpu')
+
+        # 👉🏻 Training loop / simulation takes place here
+        for et_i in range(config.episode_length):
+            # rearrange observations to be per agent, and convert to torch Variable
+            torch_obs = [Variable(torch.Tensor(np.vstack(obs[:, i])),
+                                  requires_grad=False)
+                         for i in range(model.nagents)]
+            # get actions as torch Variables
+            # 👉🏻 model.step = policy (obs -> action); choose action of each agent according to the policy
+            torch_agent_actions = model.step(torch_obs, explore=True) 
+            # convert actions to numpy arrays
+            agent_actions = [ac.data.numpy() for ac in torch_agent_actions]
+            # rearrange actions to be per environment
+            # 👉🏻 Simulate
+            actions = [[ac[i] for ac in agent_actions] for i in range(config.n_rollout_threads)]
+            next_obs, rewards, dones, infos = env.step(actions)
+
+            """
+            TODO
+            Split up teams
+            1. Share rewards based on the team
+            2. Share rewards with nearby colleague
+            
+            First, distribute reward.
+            Second, somehow change policy?
+            """
+            for rolloutNum, arr in enumerate(next_obs):
+                for agentNum, agent_obs in enumerate(arr):
+                    if env.agent_types[agentNum] == 'agent':
+                        continue
+                    
+                    for idx in range(TOTAL_AGENTS-1): # all other agents except me
+                        otherNum = idx + (1 if idx >= agentNum else 0) # other agent's index, passing me
+                        if env.agent_types[otherNum] == 'agent':
+                            continue # 'teaming' is only for predators
+                        relX = agent_obs[OTHER_OFFSET + 2*idx] # other pos X (relative)
+                        relY = agent_obs[OTHER_OFFSET + 2*idx + 1] # other pos Y (relative)
+                        
+                        # Give extra reward if close, but give penalty if far away
+                        dist = math.sqrt(relX ** 2 + relY ** 2)
+                        penalty = dist * (-1 if dist > CLOSENESS else 1)
+
+                        rewards[rolloutNum][otherNum] += penalty
+
+                        """
+                        # TODO
+                        1. Only when predator catches prey - How to detect this? 'done'?
+                        2. decide reward/penalty value, not based on the distance?
+                        3. standard of CLOSENESS? size of the map?
+                        4. How to compare with other methods
+                        """
+
+            # 👉🏻 저장 & sampling 역할; off-policy만 사용 가능
+            replay_buffer.push(obs, agent_actions, rewards, next_obs, dones)
+
+            obs = next_obs
+            t += config.n_rollout_threads
+            if (len(replay_buffer) >= config.batch_size and
+                (t % config.steps_per_update) < config.n_rollout_threads):
+
+                if config.use_gpu:
+                    model.prep_training(device='gpu')
+                else:
+                    model.prep_training(device='cpu')
+                
+                for u_i in range(config.num_updates):
+                    sample = replay_buffer.sample(config.batch_size,
+                                                  to_gpu=config.use_gpu)
+                    # 👉🏻 Update centralized critic
+                    model.update_critic(sample, logger=logger)
+                    # 👉🏻 Update policy (actor) using critic
+                    model.update_policies(sample, logger=logger)
+                    model.update_all_targets()
+
+                if config.use_gpu:
+                    model.prep_training(device='gpu')
+                else:
+                    model.prep_training(device='cpu')
+
+        # 👉🏻 End of training - get rewards
+        ep_rews = replay_buffer.get_average_rewards(
+            config.episode_length * config.n_rollout_threads)
+
+        # sum up the rewards for each type of agent!
+        predator_reward_sum = 0
+        prey_reward_sum = 0
+        num_adversaries = 10
+ 
+        for a_i, a_ep_rew in enumerate(ep_rews):
+            if a_i < num_adversaries:
+                predator_reward_sum += a_ep_rew * config.episode_length
+            else:
+                prey_reward_sum += a_ep_rew * config.episode_length
+            
+            logger.add_scalar('agent%i/mean_episode_rewards' % a_i,
+                              a_ep_rew * config.episode_length, ep_i)
+
+        logger.add_scalar('predator_reward_sum', predator_reward_sum, ep_i)
+        logger.add_scalar('prey_reward_sum', prey_reward_sum, ep_i)
+        logger.add_scalar('total_reward_sum', prey_reward_sum + predator_reward_sum, ep_i)
+
+        if ep_i % config.save_interval < config.n_rollout_threads:
+            
+            if config.use_gpu:
+                model.prep_training(device='gpu')
+            else:
+                model.prep_training(device='cpu')
+
+            os.makedirs(run_dir / 'incremental', exist_ok=True)
+            model.save(run_dir / 'incremental' / ('model_ep%i.pt' % (ep_i + 1)))
+            model.save(run_dir / 'model.pt')
+
+        avg_rew = np.array(ep_rews) * config.episode_length
+        # print(avg_rew)
+
+        # record reward in every episode
+        # format : (1, 8) python list - [agent1 reward, agent2, ..., agent8]
+        write.writerow(avg_rew) 
+
+    f.close()
+
+    model.save(run_dir / 'model.pt')
+    env.close()
+    logger.export_scalars_to_json(str(log_dir / 'summary.json'))
+    logger.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("env_id", help="Name of environment")
+    parser.add_argument("model_name",
+                        help="Name of directory to store " +
+                             "model/training contents")
+    parser.add_argument("--n_rollout_threads", default=12, type=int)
+    parser.add_argument("--buffer_length", default=int(1e6), type=int)
+    parser.add_argument("--n_episodes", default=50000, type=int)
+    parser.add_argument("--episode_length", default=25, type=int)
+    parser.add_argument("--steps_per_update", default=100, type=int)
+    parser.add_argument("--num_updates", default=4, type=int,
+                        help="Number of updates per update cycle")
+    parser.add_argument("--batch_size",
+                        default=1024, type=int,
+                        help="Batch size for training")
+    parser.add_argument("--save_interval", default=1000, type=int)
+    parser.add_argument("--pol_hidden_dim", default=128, type=int)
+    parser.add_argument("--critic_hidden_dim", default=128, type=int)
+    parser.add_argument("--attend_heads", default=4, type=int)
+    parser.add_argument("--pi_lr", default=0.001, type=float)
+    parser.add_argument("--q_lr", default=0.001, type=float)
+    parser.add_argument("--tau", default=0.001, type=float)
+    parser.add_argument("--gamma", default=0.99, type=float)
+    parser.add_argument("--reward_scale", default=100., type=float)
+    parser.add_argument("--use_gpu", action='store_true')
+
+    # arguments for simple_tag environment
+
+
+    parser.add_argument("--testnum", default=1, type=int)
+
+    config = parser.parse_args()
+
+    run(config)
